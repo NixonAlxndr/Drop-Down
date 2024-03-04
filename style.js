a0c4c8c62d44468a434f5087abf12196c9cf7e2a
NixonAlxndr <94671395+NixonAlxndr@users.noreply.github.com>
2024-03-04 13:47:49 +0700

    Add files via upload

---
+++ style.js
@@ -0,0 +1,4 @@
+const drop_down = document.querySelector('.drop-down');
+const dropDown = document.querySelector('.drop-down-menu');
+
+drop_down.addEventListener('click', ()=>dropDown.classList.toggle('active'));
